f828b55e759feca3616611c564e18d5804797a8e
coder8338 <64418056+coder8338@users.noreply.github.com>
2022-04-04 19:45:19 +0100

    Update swizzin_installer.sh

--- swizzin_installer.sh
+++ swizzin_installer.sh
@@ -2,22 +2,6 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-OLD_INSTALLS_EXIST=0
-
-check_old_installs() {
-    echo "Checking for old installs..."
-    # Create array of old installs
-    OLD_INSTALLS=(radarr sonarr sickchill jackett couchpotato nzbget sabnzbdplus ombi lidarr organizr nzbhydra2 bazarr flexget filebot synclounge medusa lazylibrarian pyload ngpost komga ombiv4 readarr overseerr requestrr updatetool flood tautulli unpackerr mylar flaresolverr)
-
-    # Loop through array
-    for i in "${OLD_INSTALLS[@]}"; do
-        # Check if install exists
-        if [ -d "/etc/services.d/$i" ]; then
-            OLD_INSTALLS_EXIST=1
-        fi
-    done
-}
-
 run_as_root() {
     if ! whoami | grep -q 'root'; then
         echo "This script must be run with sudo, please run:"
@@ -27,11 +11,6 @@ run_as_root() {
 }
 
 run_as_root
-
-if [ ! -f /etc/nginx/sites-enabled/appbox.conf ]; then
-    rm /etc/nginx/sites-enabled/default
-    wget -q https://raw.githubusercontent.com/coder8338/appbox_swizzin_installer/Ubuntu_20.04/nginx_orig.conf -O /etc/nginx/sites-enabled/appbox.conf
-fi
 
 echo 'Please enter your Ubuntu password (for the username appbox):'
 read -r USER_PASSWORD
@@ -63,100 +42,17 @@ Enjoy!
     \n\n"
 }
 
-create_service() {
-    NAME=$1
-    mkdir -p /etc/services.d/${NAME}/log
-    echo "3" >/etc/services.d/${NAME}/notification-fd
-    cat <<EOF >/etc/services.d/${NAME}/log/run
-#!/bin/sh
-exec logutil-service /var/log/appbox/${NAME}
-EOF
-    chmod +x /etc/services.d/${NAME}/log/run
-    echo "${RUNNER}" >/etc/services.d/${NAME}/run
-    chmod +x /etc/services.d/${NAME}/run
-    cp -R /etc/services.d/${NAME} /var/run/s6/services
-    kill -HUP 1
-    until [ -d "/run/s6/services/${NAME}/supervise/" ]; do
-        echo
-        echo "Waiting for s6 to recognize service..."
-        sleep 1
-    done
-    s6-svc -u /run/s6/services/${NAME}
-}
-
-cat <<EOF >/usr/lib/os-release
-NAME="Ubuntu"
-VERSION="20.04.1 LTS (Focal Fossa)"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu 20.04.1 LTS"
-VERSION_ID="20.04"
-HOME_URL="https://www.ubuntu.com/"
-SUPPORT_URL="https://help.ubuntu.com/"
-BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
-PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
-VERSION_CODENAME=focal
-UBUNTU_CODENAME=focal
-EOF
-
 mkdir -p /run/php/
 
-check_old_installs
-
-if [ $OLD_INSTALLS_EXIST -eq 1 ]; then
-    echo "Old installs detected, this will cause a conflict with the new Swizzin services."
-    echo "Would you like to remove them (y/n)?"
-    read -r REMOVE_OLD_INSTALLS
-    if [ "$REMOVE_OLD_INSTALLS" = "y" ] || [ "$REMOVE_OLD_INSTALLS" = "Y" ] || [ "$REMOVE_OLD_INSTALLS" = "yes" ]; then
-        echo "Removing old installs..."
-        for i in "${OLD_INSTALLS[@]}"; do
-            echo "Removing $i..."
-            s6-svc -d /run/s6/services/"$i" || true
-            if [ -d "/etc/services.d/$i" ]; then
-                rm -rf /etc/services.d/"$i"
-            fi
-            if [ -d "/var/run/s6/services/$i" ]; then
-                rm -rf /var/run/s6/services/"$i"
-            fi
-            if [ -d "/home/appbox/.config/${i^}" ]; then
-                rm -rf /home/appbox/.config/"${i^}"
-            fi
-            if [ -d "/var/log/appbox/$i" ]; then
-                rm -rf /var/log/appbox/"$i"
-            fi
-        done
-
-        rm -rf /home/appbox/appbox_installer
-    else
-        echo "Please remove the old installs and try again, or use this script to remove them."
-        exit 1
-    fi
+# Just do this the first time
+if [ -f /etc/systemd/system/dbus-fi.w1.wpa_supplicant1.service ] && [ ! -f /etc/systemd/system/panel.service ]; then
+    rm -rf /lib/systemd/system/*
+    rm -rf /etc/systemd/system/*
 fi
 
-sed -i 's/www-data/appbox/g' /etc/nginx/nginx.conf
-echo -e "\nUpdating mono certs..."
-cert-sync --quiet /etc/ssl/certs/ca-certificates.crt
-echo -e "\nUpdating apt packages..."
-echo >>/etc/apt/apt.conf.d/99verify-peer.conf "Acquire { https::Verify-Peer false }"
-
-rm -rf /lib/systemd/system/*.service
-rm -rf /etc/systemd/system/*.service
-
-wget -q https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -O /usr/local/bin/systemctl
-chmod +x /usr/local/bin/systemctl
-
-systemctl daemon-reload
-
-RUNNER=$(
-    cat <<EOF
-#!/bin/execlineb -P
-# Redirect stderr to stdout.
-fdmove -c 2 1
-/usr/local/bin/systemctl --init
-EOF
-)
-
-create_service 'systemd'
+echo "Upgrading system..."
+apt-get -qq update
+apt-get -qq upgrade -y
 
 echo -e "\nInstalling required packages..."
 apt-get -qq install -y git
