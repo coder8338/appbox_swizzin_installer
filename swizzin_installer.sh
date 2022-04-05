fe7c962d1950ac50f3dee04932fab7fafa3f2f23
coder8338 <64418056+coder8338@users.noreply.github.com>
2022-04-05 10:39:10 +0100

    Update swizzin_installer.sh

--- swizzin_installer.sh
+++ swizzin_installer.sh
@@ -161,6 +161,49 @@ git fetch origin overseer &>/dev/null
 git merge --no-edit origin/overseer &>/dev/null
 sed -i '/Type=exec/d' /etc/swizzin/scripts/install/overseerr.sh
 sed -i 's/# _nginx/_nginx/g' /etc/swizzin/scripts/install/overseerr.sh
+cat >/etc/swizzin/scripts/nginx/overseerr.sh <<EON
+#!/usr/bin/env bash
+
+cat > /etc/nginx/apps/overseerr.conf << EOF
+location /overseerr {
+    set \$app "overseerr";
+    # Remove /overseerr path to pass to the app
+    rewrite ^/overseerr/?(.*)$ /\$1 break;
+    proxy_pass http://127.0.0.1:5055; # NO TRAILING SLASH
+
+    # Redirect location headers
+    proxy_redirect ^ /\$app;
+    proxy_redirect /setup /\$app/setup;
+    proxy_redirect /login /\$app/login;
+
+    # Sub filters to replace hardcoded paths
+    proxy_set_header Accept-Encoding "";
+    sub_filter_once off;
+    sub_filter_types *;
+    sub_filter '\''href="/"'\'' '\''href="/\$app"'\'';
+    sub_filter '\''href="/login"'\'' '\''href="/\$app/login"'\'';
+    sub_filter '\''href:"/"'\'' '\''href:"/\$app"'\'';
+    sub_filter '\''/_next'\'' '\''/\$app/_next'\'';
+    sub_filter '\''/api/v1'\'' '\''/\$app/api/v1'\'';
+    sub_filter '\''/login/plex/loading'\'' '\''/\$app/login/plex/loading'\'';
+    sub_filter '\''/images/'\'' '\''/\$app/images/'\'';
+    sub_filter '\''/apple-'\'' '\''/\$app/apple-'\'';
+    sub_filter '\''/favicon'\'' '\''/\$app/favicon'\'';
+    sub_filter '\''/logo.png'\'' '\''/\$app/logo.png'\'';
+    sub_filter '\''/logo_full.svg'\'' '\''/\$app/logo_full.svg'\'';
+    sub_filter '\''/logo_stacked.svg'\'' '\''/\$app/logo_stacked.svg'\'';
+    sub_filter '\''/site.webmanifest'\'' '\''/\$app/site.webmanifest'\'';
+}
+EOF
+
+cat > /opt/overseerr/env.conf << EOF
+
+# specify on which interface to listen, by default overseerr listens on all interfaces
+HOST=127.0.0.1
+EOF
+
+systemctl try-restart overseerr
+EON
 sed -i '/Continue setting up user/d' /etc/swizzin/scripts/box
 
 /etc/swizzin/setup.sh --unattend nginx panel radarr sonarr --user appbox --pass "$USER_PASSWORD"
